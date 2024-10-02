6878a6784a3ad6e172c43394fe488fc22c5c4449
Thanh Minh Nguyen <minh74dn@gmail.com>
2024-10-02 18:34:33 +0700

    login, authen, blog

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from blog import  models
+from blog.database import engine
+from blog.routers import blog, user, authentication
+
+app = FastAPI()
+
+models.Base.metadata.create_all(engine)
+
+app.include_router(authentication.router)
+app.include_router(blog.router)
+app.include_router(user.router)
+
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
