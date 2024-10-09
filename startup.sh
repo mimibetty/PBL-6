315e8ba9341c21e1642ae0d2032d82b19d838df0
Thanh Minh Nguyen <minh74dn@gmail.com>
2024-10-09 20:29:47 +0700

    fix startup

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/site/wwwroot
+cd /home/site/wwwroot/app
 export PYTHONPATH=/home/site/wwwroot/app
-gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --timeout 600
+gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --timeout 600
 
