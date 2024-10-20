7e997d624d753a40755130a02c0b86efde4c930e
levantiendat <levan040669@gmail.com>
2024-10-21 01:10:39 +0700

    fix title UI for button

--- frontend/src/components/views/detailLocation_Place.vue
+++ frontend/src/components/views/detailLocation_Place.vue
@@ -1,16 +1,14 @@
 <template>
     <div class="main-container">
       <div class="flex-row-cdcf">
-        <div class="rectangle">
-            <a href="#" class="home">Home</a>
-          </div>
         <div class="image"></div>
         <span class="travel-tips">Travel Tips</span>
         <button class="union" @click="toggleMenu"></button>
-        <a href="#" class="travel-itinerary" >Travel Itinerary</a>
-        <a href="#" class="tour-proposal"  >Tour Proposal</a>
-        <a href="#" class="forum"  >Forum</a>
-        <a href="#" class="trips" >Trips</a>
+        <button class="home" @click="alert('Đã nhấn Home')">Home</button>
+        <button class="travel-itinerary"  @click.prevent="alert('Đã nhấn')">Travel Itinerary</button>
+        <button class="tour-proposal"  @click.prevent="alert('Đã nhấn Proposal')">Tour Proposal</button>
+        <button class="forum"  @click.prevent="alert('Đã nhấn Forum')">Forum</button>
+        <button class="trips"  @click.prevent="alert('Đã nhấn Trip')">Trips</button>
         <div class="menu-container" v-if="isMenuVisible">
               <div class="menu-options">
                 <button class="menu-button my-trips">My Trips</button>
@@ -203,33 +201,7 @@ button {
   margin: -2px 0 0 0;
   z-index: 30;
 }
-.rectangle {
-  position: absolute;
-  width: 9%;
-  height: 100%;
-  top: 0;
-  left: 40%;
-  background: #13357b;
-  z-index: 20;
-}
-.home {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: absolute;
-  width: 5%;
-  height: 20%;
-  top: 40%;
-  left: 50%;
-  color: #f4fff8;
-  font-family: Poppins, var(--default-font-family);
-  font-size: 1.75vw;
-  font-weight: 700;
-  line-height: 150%;
-  text-align: center;
-  white-space: nowrap;
-  z-index: 21;
-}
+
 .image {
   position: absolute;
   width: 8%;
@@ -327,71 +299,114 @@ button {
 }
 .travel-itinerary {
   display: flex;
-  align-items: flex-start;
-  justify-content: flex-start;
+  align-items: center;
+  justify-content: center;
   position: absolute;
-  height: 20%;
-  top: 40%;
-  left: 70%;
-  color: #1836ba;
+  width: 14%;
+  height: 100%;
+  top: 0%;
+  left: 68%;
+  background: none;
+  color: #13357b;
   font-family: Poppins, var(--default-font-family);
-  font-size: 1.5vw;
+  font-size: 1.75vw;
   font-weight: 700;
-  line-height: 100%;
-  text-align: left;
+  line-height: 150%;
+  text-align: center;
+  border: none;
   white-space: nowrap;
-  z-index: 17;
+  z-index: 21;
 }
+
+.home {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  width: 10%;
+  height: 100%;
+  top: 0%;
+  left: 40%;
+  background: #13357b;
+  color: #f4fff8;
+  font-family: Poppins, var(--default-font-family);
+  font-size: 1.75vw;
+  font-weight: 700;
+  line-height: 150%;
+  text-align: center;
+  
+  white-space: nowrap;
+  z-index: 21;
+}
+
 .tour-proposal {
   display: flex;
-  align-items: flex-start;
-  justify-content: flex-start;
+  align-items: center;
+  justify-content: center;
   position: absolute;
-  height: 20%;
-  top: 40%;
-  left: 52%;
-  color: #1836ba;
+  width: 12%;
+  height: 100%;
+  top: 0%;
+  left: 50%;
+  background: none;
+  color: #13357b;
   font-family: Poppins, var(--default-font-family);
-  font-size: 1.5vw;
+  font-size: 1.75vw;
   font-weight: 700;
-  line-height: 100%;
-  text-align: left;
+  line-height: 150%;
+  text-align: center;
+  border: none;
   white-space: nowrap;
-  z-index: 15;
+  z-index: 21;
 }
 .forum {
   display: flex;
-  align-items: flex-start;
-  justify-content: flex-start;
+  align-items: center;
+  justify-content: center;
   position: absolute;
-  height: 20%;
-  top: 40%;
-  left: 83%;
-  color: #1836ba;
+  width: 6%;
+  height: 100%;
+  top: 0%;
+  left: 82%;
+  background: none;
+  color: #13357b;
   font-family: Poppins, var(--default-font-family);
-  font-size: 1.5vw;
+  font-size: 1.75vw;
   font-weight: 700;
-  line-height: 100%;
-  text-align: left;
+  line-height: 150%;
+  text-align: center;
+  border: none;
   white-space: nowrap;
-  z-index: 18;
+  z-index: 21;
 }
 .trips {
   display: flex;
-  align-items: flex-start;
-  justify-content: flex-start;
+  align-items: center;
+  justify-content: center;
   position: absolute;
-  height: 20%;
-  top: 40%;
-  left: 65%;
-  color: #1836ba;
+  width: 6%;
+  height: 100%;
+  top: 0%;
+  left: 62%;
+  background: none;
+  color: #13357b;
   font-family: Poppins, var(--default-font-family);
-  font-size: 1.5vw;
+  font-size: 1.75vw;
   font-weight: 700;
-  line-height: 100%;
-  text-align: left;
+  line-height: 150%;
+  text-align: center;
+  border: none;
   white-space: nowrap;
-  z-index: 16;
+  z-index: 21;
+}
+
+.home:hover, 
+.tour-proposal:hover, 
+.trips:hover, 
+.travel-itinerary:hover, 
+.forum:hover {
+  background-color: #8c9aec;
+  color: #dbe4f5;
 }
 .line {
   position: absolute;
