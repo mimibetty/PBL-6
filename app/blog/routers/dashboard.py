659f726e1f2d95bbe94b541be3afec71929c38af
karuhame <tomtran2k3@gmail.com>
2024-12-19 12:20:46 +0700

    feat(dashboard): get user/business by days + by month

--- app/blog/routers/dashboard.py
+++ app/blog/routers/dashboard.py
@@ -27,8 +27,26 @@ def search_by_name_of_destination_and_city(
     }
 
     return results
-
-@router.get("/usercounts/month/{year}", response_model=List[schemas.UserCountByMonth])
+@router.get("/usercounts/by_month/{year}", response_model=List[schemas.UserCountByMonth],
+    description=(
+        "### Retrieve user counts by month for a given year.\n\n"
+        "- **Parameters**:\n"
+        "    - **year**: The year for which to retrieve user counts (e.g., 2023).\n"
+        "    - **is_business**: Specify if you want counts for business users (true) or guest users (false).\n\n"
+        "- **Example**:\n"
+        "    - Request: `/usercounts/by_month/2023?is_business=true`\n\n"
+        "    - Response:\n"
+        "    ```json\n"
+        "    [\n"
+        "        {\"month\": 1, \"user_count\": 15},\n"
+        "        {\"month\": 2, \"user_count\": 20},\n"
+        "        {\"month\": 3, \"user_count\": 30},\n"
+        "        ...\n"
+        "    ]\n"
+        "    ```\n\n"
+        "- **Explanation**: This provides the number of business users registered for each month in the year 2023.\n"
+    )
+)
 def read_user_counts_by_month(
     year: int,
     is_business: bool,
@@ -36,7 +54,26 @@ def read_user_counts_by_month(
 ):
     return dashboard.get_account_counts_by_month(db=db, year=year, is_business= is_business)
 
-@router.get("/usercounts/{year}/{month}/days", response_model=List[schemas.UserCountDetail])
+@router.get("/usercounts/by_day/{month}/{year}", response_model=List[schemas.UserCountDetail],
+    description=(
+        "### Retrieve user counts by day for a given month and year.\n\n"
+        "- **Parameters**:\n"
+        "  - **year**: The year for which to retrieve user counts (e.g., 2023).\n"
+        "  - **month**: The month for which to retrieve user counts (1-12).\n"
+        "  - **is_business**: Specify if you want counts for business users (true) or guest users (false).\n\n"
+        "- **Example**:\n"
+        "  - Request: `/usercounts/by_day/3/2023?is_business=false`\n"
+        "  - Response:\n"
+        "```json\n"
+        "[\n"
+        "    {\"day\": 1, \"user_count\": 2},\n"
+        "    {\"day\": 2, \"user_count\": 5},\n"
+        "    {\"day\": 3, \"user_count\": 3},\n"
+        "    ...\n"
+        "]\n"
+        "```\n"
+        "- **Explanation**: This provides the number of guest users registered for each day in March 2023.\n"
+    ))
 def read_user_counts_by_day(
     year: int,
     month: int,
@@ -49,7 +86,7 @@ def read_user_counts_by_day(
 
 @router.get("/business/stacked_review",
             description=(
-                "### Get review_count of 1 dest based on: month + review rating\n\n"
+                "### Get review_count of 1 dest based on: month + review rating\n"
                 "- **Example**: destination_id = 6, year = 2024 \n\n"
                 "{ \n\n"
                 ' "1": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],\n\n'
@@ -57,7 +94,7 @@ def read_user_counts_by_day(
                 ' "3": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 4],\n\n'
                 ' "4": [0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 1],\n\n'
                 ' "5": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],\n\n'
-                "}\n\n"
+                "}\n"
                 "- **Explain:** tháng 11 có : 1 đánh giá 1 sao, 2 đánh giá 3 sao, 1 đánh giá 5 sao, ... \n\n"
                 "- **Format**: result[s][mth]: tổng reivew s star trong tháng m"
             ))
